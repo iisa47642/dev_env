48bd0750aa3d463d80910f8045d8b4c423947855
iisa47642 <136353118+iisa47642@users.noreply.github.com>
2024-10-04 19:45:10 +0300

    Main-app the ready

--- app/main/views.py
+++ app/main/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели Home',
     }
+    
     return render(request, 'main/index.html', context)
 
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том, почему этот магазин такой классный'
+    }
+    return render(request, 'main/about.html', context)

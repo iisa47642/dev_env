f94e04a2301e7d714771d038682b0bbe595b3363
iisa47642 <136353118+iisa47642@users.noreply.github.com>
2024-10-05 10:54:26 +0300

    add bd and app goods

---
+++ app/goods/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
